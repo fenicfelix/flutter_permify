fd32e19934045c5e8e6d2b0f81a2713b41fc4fb3
Felix Ogucha <fenicfelix@gmail.com>
2025-02-16 06:16:15 +0000

    Introduces permission type for easier use for cases where permissions are not granted explicitly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1
 
 * Launches Permify with basic features to manage permissions and roles
+
+## 0.0.2
+
+* Introduces permissionType for easier use for cases where permissions are not granted explicitly
--- lib/permify.dart
+++ lib/permify.dart
@@ -6,6 +6,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class Permify {
   /// Permissions and roles are stored in SharedPreferences
   static const String permissionsKey = 'flutter_permify_permissions';
+  static const String permissionTypeKey = 'flutter_permify_permission_type';
   static const String rolesKey = 'flutter_permify_roles';
 
   /// Set the permissions for the current user
@@ -49,6 +50,54 @@ class Permify {
       // Handle exceptions
       print('Error checking permission: $e');
       throw Exception('Failed to check permission');
+    }
+  }
+
+  /// Set the permission type for the current user
+  static Future<void> setPermissionType(String permissionType) async {
+    /// Check if the permission type is null or empty
+    if (permissionType.isEmpty) {
+      throw ArgumentError('Permission type cannot be null or empty');
+    }
+
+    /// Save the permission type to SharedPreferences once it is validated
+    /// This method is useful for setting the type of permission the user has
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString(permissionTypeKey, permissionType);
+    } catch (e) {
+      // Handle exceptions
+      print('Error saving permission type: $e');
+      throw Exception('Failed to save permission type');
+    }
+  }
+
+  /// Get the permission type for the current user
+  /// This method is useful for checking the type of permission the user has
+  static Future<String> getPermissionType() async {
+    /// Get the permission type from SharedPreferences
+    try {
+      final SharedPreferences prefs = await SharedPreferences.getInstance();
+      return prefs.getString(permissionTypeKey) ?? '';
+    } catch (e) {
+      // Handle exceptions
+      print('Error getting permission type: $e');
+      throw Exception('Failed to get permission type');
+    }
+  }
+
+  /// Check if the current user has a specific permission type
+  /// This method is useful for checking if the user has a specific permission type
+  /// e.g. 'admin', 'user', 'moderator', etc.
+  static Future<bool> hasPermissionType(String permissionType) async {
+    /// Check if the permission type is the same as the saved permission type
+    try {
+      final String savedPermissionType = await getPermissionType();
+      return savedPermissionType == permissionType;
+    } catch (e) {
+      // Handle exceptions
+      print('Error checking permission type: $e');
+      throw Exception('Failed to check permission type');
     }
   }
 
